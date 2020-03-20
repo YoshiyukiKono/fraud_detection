50a47ecd06476a8d923002a2a0d7727a75fbff1d
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2020-03-20 07:37:20 +0000

    Updated files.

--- r_scripts/setup.r
+++ r_scripts/setup.r
@@ -2,4 +2,20 @@ install.packages("pROC", dependencies = TRUE)
 install.packages("microbenchmark", dependencies = TRUE)
 install.packages("gbm", dependencies = TRUE)
 install.packages("xgboost", dependencies = TRUE)
-install.packages("lgb", dependencies = TRUE)
+devtools::install_github("Laurae2/lgbdl")
+#lgbdl::lgb.dl(commit = "master",
+#              compiler = "vs", # Remove this for MinGW + GPU installation
+#              repo = "https://github.com/Microsoft/LightGBM", 
+#              cores = 4,
+#              R35 = TRUE,
+#              use_gpu = TRUE)
+
+# Error in lgbdl::lgb.dl(commit = "master", compiler = "vs", repo = "https://github.com/Microsoft/LightGBM",  : 
+#  unused argument (R35 = TRUE)
+
+
+lgbdl::lgb.dl(commit = "master",
+              compiler = "vs", # Remove this for MinGW + GPU installation
+              repo = "https://github.com/Microsoft/LightGBM", 
+              cores = 4,
+              use_gpu = TRUE)
